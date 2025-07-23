b3f1387d85d44a0e01223d7bfd6f15ed1efa925f
Devansh Jain <93151123+Devansh015@users.noreply.github.com>
2025-07-23 16:16:01 -0400

    google tag added for analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,20 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-E6S3LZ1280"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-E6S3LZ1280');
+            `,
+          }}
+        />
+      </head>
       <body className={inter.className}>
         <ThemeProvider>{children}</ThemeProvider>
       </body>
